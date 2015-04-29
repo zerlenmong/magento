1c87c47e5821e8d0e94606ae7e207417a7dbcfc3
louis.zeng <louis.zeng@silksoftware.com>
2015-04-29 11:55:17 +0800

    new project

--- app/code/community/PlanetPayment/Upop/sql/upop_setup/upgrade-1.0.13-1.0.17.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$installer = $this;
-$installer->startSetup();
-
-$installer->getConnection()->addColumn($installer->getTable('sales/order'), 'dbt_status',
-    'tinyint DEFAULT NULL');
-
-$installer->endSetup();
